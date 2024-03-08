9881f6cc599a17887e5b3c03e22c295604fa4fe3
Juan Caceres <dnijuancaceres@gmail.com>
2024-03-08 16:02:38 -0300

    Create function RandomColor()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Accordian from './components/accordian';
+import RandonColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
+      /*Import components*/
       <Accordian/>
+      <RandonColor/>
     </div>
   );
 }
