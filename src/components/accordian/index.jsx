b83dafc22d53ec07aa0b5f259482a4ef3940448d
Juan Caceres <dnijuancaceres@gmail.com>
2024-03-07 22:23:30 -0300

    Funtion handleSingleSelection, show and disguise content

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -5,14 +5,28 @@ export default function Accordian()
 {
     const [selected, setSelected] = useState(null)
 
+    function handleSingleSelection(getCurrentId){
+        setSelected(getCurrentId === selected ? null : getCurrentId)
+    }
+
+    console.log(selected);
+
     return <div className="wrapper">
         <div className="accordian">
             {
                 data && data.length > 0 ?
-                data.map(dataItem => <div className="item">
-                    <div className="title">
+                data.map(dataItem => 
+                <div className="item">
+                    <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
                         <h3>{dataItem.question}</h3>
                         <span>+</span>
+                        <div>
+                            {
+                                selected === dataItem.id ?(
+                                <div className="content">{dataItem.answer}</div>
+                                ): null
+                            }
+                        </div>
                     </div>
                 </div>) 
                 : <div>No data found it</div>
