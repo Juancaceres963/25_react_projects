93df8b36ec802a10836d597b1f886c6a959350d5
Juan Caceres <dnijuancaceres@gmail.com>
2024-03-11 16:52:33 -0300

    star-rating completed

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import {FaStar} from "react-icons/fa"
+import "./styles.css"
 
 export default function StarRanting({noOfStars = 5}){
 
@@ -8,29 +9,35 @@ export default function StarRanting({noOfStars = 5}){
 
     function handleClick(currentIndex){
         console.log(currentIndex)
+        setRating(currentIndex)
     }
 
     function handleMouseEnter(currentIndex){
         console.log(currentIndex)
+        setHover(currentIndex)
     }
 
-    function handleMouseLeave(currentIndex){
-        console.log(currentIndex)
+    function handleMouseLeave(){
+        console.log(rating)
+        setHover(rating)
     }
 
-    return <div className="star-rating">
-        {
-            [...Array(noOfStars)].map((_,index)=> {
-                index += 1
+    return (
+    <div className="star-rating">
+        {[...Array(noOfStars)].map((_,index)=> {
+            index += 1;
 
-                return <FaStar
-                key={index}
-                onClick={() => handleClick(index)}
-                onMouseMove={() => handleMouseEnter(index)}
-                onMouseLeave={() => handleMouseLeave(index)}
-                size={40}
+            return (
+                <FaStar
+                    key={index}
+                    className={index <= (hover || rating) ? "active" : "inactive"}
+                    onClick={() => handleClick(index)}
+                    onMouseMove={() => handleMouseEnter(index)}
+                    onMouseLeave={() => handleMouseLeave()}
+                    size={40}
                 />
-            })
-        }
+            );
+        })}
     </div>
+    )
 }
