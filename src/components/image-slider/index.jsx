c7a376bb6222d61e237714ab3a1de1b2f8d9a29e
Juan Caceres <dnijuancaceres@gmail.com>
2024-03-12 13:03:13 -0300

    Logic return elements withough styles

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
-export default function ImageSlider({ url, limit , page }) {
+export default function ImageSlider({ url, limit, page }) {
   const [image, setImage] = useState([]);
   const [currentSlider, setCurrentSlider] = useState(0);
   const [errorMsg, setErrorMsg] = useState(null);
@@ -13,9 +14,9 @@ export default function ImageSlider({ url, limit , page }) {
       const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
       const data = await response.json();
 
-      if (data){
-        setImage(data)
-        setLoading(false)
+      if (data) {
+        setImage(data);
+        setLoading(false);
       }
     } catch (e) {
       setErrorMsg(e.message);
@@ -26,17 +27,41 @@ export default function ImageSlider({ url, limit , page }) {
   useEffect(
     () => {
       if (url !== "") fetchImages(url);
-    },{ url });
+    },
+    { url }
+  );
 
-    console.log(image)
+  console.log(image);
 
-  if (loading){
-    return <div>Loading data. Please wait...</div>
+  if (loading) {
+    return <div>Loading data. Please wait...</div>;
   }
 
-  if (errorMsg !== null){
-    return <div>Error occured | {errorMsg}</div>
+  if (errorMsg !== null) {
+    return <div>Error occured | {errorMsg}</div>;
   }
 
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill className="arrow arrow-left" />
+      {image && image.length
+        ? image.map((imageItem) => (
+            <img
+              key={imageItem.id}
+              src={imageItem.download_url}
+              alt={imageItem.download_url}
+              className="current-image"
+            />
+          ))
+        : null}
+      <BsArrowRightCircleFill className="arrow arrow-right" />
+      <span>
+        {image && image.length
+          ? image.map((_, index) => (
+              <button key={index} className="current-indicator"></button>
+            ))
+          : null}
+      </span>
+    </div>
+  );
 }
