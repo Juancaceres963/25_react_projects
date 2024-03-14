0b0b91dea5450701dd0b35e1fc26373d98dbe10e
Juan Caceres <dnijuancaceres@gmail.com>
2024-03-13 21:18:20 -0300

    Logic handleToggleChildren with return jsx

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import MenuList from "./menu-list";
 
+export default function MenuItem({ item }) {
+  const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
 
-export default function MenuItem({item}){
-    return <li>
+  function handleToggleChildren(getCurrentLabel) {
+    setDisplayCurrentChildren({
+      ...displayCurrentChildren,
+      [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
+    });
+  }
+
+  console.log(displayCurrentChildren);
+
+  return (
+    <li>
+      <div style={{ display: "flex", gap: "20px" }}>
         <p>{item.label}</p>
-        {
-            item && item.children && item.children.length > 0 ?
-            <MenuList list={item.children}/>
-            : null
-        }
+        {item && item.children && item.children.length ? (
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {displayCurrentChildren[item.label] ? "-" : "+"}
+          </span>
+        ) : null}
+      </div>
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayCurrentChildren[item.label] ? (
+        <MenuList list={item.children} />
+      ) : null}
     </li>
+  );
 }
--- src/components/tree-view/menu-list.jsx
+++ src/components/tree-view/menu-list.jsx
@@ -4,7 +4,7 @@ export default function MenuList({ list = [] }) {
   return (
     <ul className="menu-list-container">
       {list && list.length
-        ? list.map(listItem => <MenuItem item={listItem}/>)
+        ? list.map((listItem) => <MenuItem item={listItem} />)
         : null}
     </ul>
   );
