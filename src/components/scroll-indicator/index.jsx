bd2a0801f793f762ee3c11e8daeaebcdacc7757d
Juan Caceres <dnijuancaceres@gmail.com>
2024-03-15 11:30:22 -0300

    CodeJXS and Styles of dataItem

--- src/components/scroll-indicator/index.jsx
+++ src/components/scroll-indicator/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./styles.css";
 
 export default function ScrollIndicator({ url }) {
   const [data, setData] = useState([]);
@@ -11,7 +12,10 @@ export default function ScrollIndicator({ url }) {
       const response = await fetch(getUrl);
       const data = await response.json();
 
-      console.log(data);
+      if (data && data.products && data.products.length > 0) {
+        setData(data.products);
+        setLoading(false);
+      }
     } catch (e) {
       console.log(e);
       setErrorMessage(e.message);
@@ -22,6 +26,19 @@ export default function ScrollIndicator({ url }) {
     fetchData(url);
   }, [url]);
 
-  return;
-  <div></div>;
+  console.log(data, loading);
+
+  return (
+    <div>
+      <h1>Custon Scroll Indicator</h1>
+      <div className="data-container">
+        {data && data.length > 0
+          ? data.map((dataItem) => <div className="item-container">
+            <p className="dataItem-title">{dataItem.title}</p>
+            <p className="dataItem-brand">{`~ ${dataItem.brand} ~`}</p>
+            </div>)
+          : null}
+      </div>
+    </div>
+  );
 }
