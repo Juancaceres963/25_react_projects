ab294daa506559d3a079a91f82913c99ed281aae
Juan Caceres <dnijuancaceres@gmail.com>
2024-03-19 09:59:20 -0300

    First styles and onClose function

--- src/components/custom-model-popup/modal-test.jsx
+++ src/components/custom-model-popup/modal-test.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Modal from "./modal";
+import "./modal.css";
 
 export default function ModalTest() {
   const [showModalPopup, setShowModalPopup] = useState(false);
@@ -9,13 +10,21 @@ export default function ModalTest() {
     setShowModalPopup(!showModalPopup);
   }
 
+  function onClose() {
+    setShowModalPopup(false);
+  }
+
   return (
     <div>
       <button onClick={handleToggleModalPopup}>Open Modal Popup</button>
-      {showModalPopup && <Modal
-      header={<div>Modal Customer Header</div>}
-      body={<div>Modal Customer Body</div>}
-      footer={<h3>Modal Customer Footer</h3>}/>}
+      {showModalPopup && (
+        <Modal
+          onClose={onClose}
+          header={<div>Modal Customer Header</div>}
+          body={<div>Modal Customer Body</div>}
+          footer={<h3>Modal Customer Footer</h3>}
+        />
+      )}
     </div>
   );
 }
