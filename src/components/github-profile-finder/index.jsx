9c1eba3ba0f087d830b2d5095f717660c880123d
Juan Caceres <dnijuancaceres@gmail.com>
2024-03-20 19:45:43 -0300

    useState userData and loading

--- src/components/github-profile-finder/index.jsx
+++ src/components/github-profile-finder/index.jsx
@@ -2,13 +2,21 @@ import { useEffect, useState } from "react";
 
 export default function GithubProfileFinder() {
   const [userName, setUserName] = useState("Juancaceres963");
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   function handleSudmit() {}
 
   async function fetchGithubUserData() {
+    setLoading(true)
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
+
+    if (data){
+        setUserData(userData)
+        setLoading(false)
+    }
 
     console.log(data);
   }
@@ -16,6 +24,10 @@ export default function GithubProfileFinder() {
   useEffect(() => {
     fetchGithubUserData();
   }, []);
+
+  if (loading){
+    return <h1>Loading data !! Please wait.</h1>
+  }
 
   return (
     <div className="github-profile-container">
