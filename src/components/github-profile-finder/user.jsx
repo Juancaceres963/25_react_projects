8c155846c32a84d7a047177dce89a32afe4ff10d
Juan Caceres <dnijuancaceres@gmail.com>
2024-03-25 21:39:13 -0300

    ClasName name-container

--- src/components/github-profile-finder/user.jsx
+++ src/components/github-profile-finder/user.jsx
@@ -16,7 +16,7 @@ export default function User({ user }) {
       <div>
         <img src={avatar_url} className="avatar" alt="User" />
       </div>
-      <div>
+      <div className="name-container">
         <a href={`https://github.com/${login}`}>{name || login}</a>
         <p>
           User joined on :{" "}
@@ -25,17 +25,19 @@ export default function User({ user }) {
           })} ${createData.getFullYear()}`}
         </p>
       </div>
-      <div>
-        <p>Public Repos</p>
-        <p>{public_repos}</p>
-      </div>
-      <div>
-        <p>Followers</p>
-        <p>{followers}</p>
-      </div>
-      <div>
-        <p>Following</p>
-        <p>{following}</p>
+      <div className="profile-info">
+        <div>
+          <p>Public Repos</p>
+          <p>{public_repos}</p>
+        </div>
+        <div>
+          <p>Followers</p>
+          <p>{followers}</p>
+        </div>
+        <div>
+          <p>Following</p>
+          <p>{following}</p>
+        </div>
       </div>
     </div>
   );
